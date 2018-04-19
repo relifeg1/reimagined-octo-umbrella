4d3dc488dacc778e1c3453c61120171caa681629
relifeg1 <relife.g1@gmail.com>
2018-04-19 21:36:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace Bot_Discord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yo");
+            Console.WriteLine(Utilities.GetAlert("TEST"));
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Bot_Discord
+{
+    class Utilities
+    {
+
+        private static Dictionary<string, string> alerts;
+
+        internal static void GetAlert(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        static Utilities()
+        {
+
+            string json = File.ReadAllText("SystemLong/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>(); 
+
+        }
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
+        
+      
+    }
+}
